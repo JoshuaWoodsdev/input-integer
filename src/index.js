3179153f6805616af63da8dc4ab0c2e0d3963c93
Joshua Woods <nordev@protonmail.com>
2025-10-01 16:21:11 -0500

    created template

--- src/index.js
+++ src/index.js
@@ -1,7 +1,30 @@
 //logic goes here
 module.exports = input_integer;
 
+const sheet = new CSSStyleSheet();
+const theme = get_theme();
+sheet.replaceSync(theme);
+
 function input_integer() {
-  const name = `input-integer`;
-  return name;
+  const el = document.createElement("div");
+  const shadow = el.attachShadow({ mode: "closed" });
+  shadow.innerHTML = `
+    <input></input>
+`;
+  shadow.adoptedStyleSheets = [sheet];
+  return el;
+}
+
+function get_theme() {
+  return `
+    input {
+        padding: 4px 8px;
+        background-color: blue;
+        border-radius: 8px;
+    }
+    input:focus {
+        outline: none;
+        border: 2px solid red;
+    }
+`;
 }
